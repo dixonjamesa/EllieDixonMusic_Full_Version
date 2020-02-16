8e6f43c8fa467f8d7041e5ef689571bdfa3ec45a
dixonjamesa <dixonjamesa@gmail.com>
2020-02-16 18:14:46 +0000

    try gig calendar instead

--- quickstart.php
+++ quickstart.php
@@ -58,31 +58,3 @@ function getClient()
     return $client;
 }
 
-
-// Get the API client and construct the service object.
-$client = getClient();
-$service = new Google_Service_Calendar($client);
-
-// Print the next 10 events on the user's calendar.
-$calendarId = 'primary';
-$optParams = array(
-  'maxResults' => 10,
-  'orderBy' => 'startTime',
-  'singleEvents' => true,
-  'timeMin' => date('c'),
-);
-$results = $service->events->listEvents($calendarId, $optParams);
-$events = $results->getItems();
-
-if (empty($events)) {
-    print "No upcoming events found.\n";
-} else {
-    print "Upcoming events:\n";
-    foreach ($events as $event) {
-        $start = $event->start->dateTime;
-        if (empty($start)) {
-            $start = $event->start->date;
-        }
-        printf("%s (%s)\n", $event->getSummary(), $start);
-    }
-}
